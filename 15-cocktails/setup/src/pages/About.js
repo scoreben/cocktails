d5b0e4bad646e5b36a8452c443a2214da9319169
scoreben <96953045+scoreben@users.noreply.github.com>
2022-04-01 01:08:59 +0300

    cocktails

---
+++ 15-cocktails/setup/src/pages/About.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+ 
+        The Soviet Union, officially the Union of Soviet Socialist Republics ( USSR ),
+        was a socialist state that spanned Eurasia during its existence from 1922 to 1991.
+        It was nominally a federal union of multiple national republics; in practice
+        its government and economy were highly centralized until its final years.
+ </p>
+  
+  
+  
+  
+  
+    </section>
+  )
+}
+
+export default About
